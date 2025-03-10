403fcff0cf14a2d1219c596a0ae6967ecf9b8341
tin nguyen <tin.nm@cyberlogitec.com>
2025-03-10 16:23:53 +0700

    feat:(set-up) config rule code

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
+    project: 'tsconfig.eslint.json',
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    'airbnb-base',
+    'airbnb-typescript/base'
   ],
   root: true,
   env: {
@@ -17,9 +17,32 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    "import/prefer-default-export": "off",
+    "object-curly-newline": "off",
+    "linebreak-style": "off",
+    "import/no-cycle": "off",
+    "arrow-body-style": "off",
+    "@typescript-eslint/no-useless-constructor": "off",
+    "@typescript-eslint/indent": [
+      "off"
+    ],
+    "class-methods-use-this": [
+      "off"
+    ],
+    "@typescript-eslint/explicit-function-return-type": [
+      "error",
+      {
+        "allowExpressions": false,
+        "allowDirectConstAssertionInArrowFunctions": true,
+        "allowConciseArrowFunctionExpressionsStartingWithVoid": true
+      }
+    ],
+    "@typescript-eslint/return-await": [
+      "off",
+    ],
+    "max-classes-per-file": "off",
+    "operator-linebreak": "off",
+    "implicit-arrow-linebreak": "off",
+    "newline-per-chained-call": "off",
   },
 };
